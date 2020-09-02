ba47280179977359ae3f911c55dca723339d59c2
stwaldo <stwaldo@gmail.com>
2020-09-02 10:16:18 -0400

    changing head to /main

--- Sources/RnaTranscription/RnaTranscription.swift
+++ Sources/RnaTranscription/RnaTranscription.swift
@@ -7,7 +7,7 @@ struct Nucleotide {
     init(_ dna: String) {
         self.dna = dna
     }
-    // function to transcribe dna to rna
+    // function to transcribe dna to rna by passing to a function
     func complementOfDNA() -> String {
         let transcription:[Character: Character] = ["G":"C", "C":"G", "T":"A", "A":"U"]
         var rna: String
