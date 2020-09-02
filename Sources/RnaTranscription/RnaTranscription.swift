2b70c7b97a4922a1988dcf025995478d0bf3774b
stwaldo <stwaldo@gmail.com>
2020-09-02 10:04:36 -0400

    Initial commit

---
+++ Sources/RnaTranscription/RnaTranscription.swift
@@ -0,0 +1,24 @@
+//Solution goes in Sources
+import Foundation
+
+struct Nucleotide {
+    var dna: String
+    
+    init(_ dna: String) {
+        self.dna = dna
+    }
+    // function to transcribe dna to rna
+    func complementOfDNA() -> String {
+        let transcription:[Character: Character] = ["G":"C", "C":"G", "T":"A", "A":"U"]
+        var rna: String
+        for nuc in self.dna {
+            do {
+                try rna.append(transcription[nuc]!)
+            } catch {
+                return "\(transcription[nuc]) is not a valid Nucleotide"
+            }
+        }
+
+        return rna
+    }
+}
